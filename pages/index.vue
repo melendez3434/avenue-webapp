a60318d38bdf8c7f689fb00c2e588b18aa121a0c
Pepe García <pgarciag93@gmail.com>
2020-09-03 15:18:15 +0200

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
 <template>
-  <div class="mx-auto flex-1 flex flex-col items-center justify-center">Index Page</div>
+  <div class="mx-auto flex-1 flex flex-col items-center justify-center text-avenue-white">
+    <Navbar />
+    IndexPage
+  </div>
 </template>
 
 <script>
+import Navbar from '@/components/commons/Navbar'
+
 export default {
   name: 'IndexPage',
+
+  auth: false,
+
+  components: {
+    Navbar,
+  },
 }
 </script>
