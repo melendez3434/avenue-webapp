15ec25604ae53f766cb2269cea859d2b4ac5f24a
Pepe García <pgarciag93@gmail.com>
2020-09-30 15:32:31 +0200

    upcoming

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="min-h-screen bg-avenue-black text-white">
     <Navbar />
+
+    <div class="container mx-auto mt-5 flex items-center space-x-8">
+      <nuxt-link :to="{ name: 'index' }" class="text-3xl text-avenue-grey">Live</nuxt-link>
+      <nuxt-link :to="{ name: 'upcoming' }" class="text-3xl text-avenue-grey">
+        Upcoming
+      </nuxt-link>
+    </div>
+
     <nuxt />
   </div>
 </template>
@@ -16,3 +24,8 @@ export default {
   },
 }
 </script>
+<style>
+.nuxt-link-exact-active {
+  @apply text-avenue-white-light;
+}
+</style>
--- pages/upcoming.vue
+++ pages/upcoming.vue
@@ -1,9 +1,52 @@
 <template>
-  <div class="container mx-auto">
-    <h1 class="text-2xl text-avenue-white-light">Upcoming</h1>
-
+  <div class="container mx-auto pb-12">
     <div class="flex flex-col space-y-4 mt-8">
       <EventListItem v-for="event in events" :key="event.id" :event="event" />
+    </div>
+
+    <div class="mt-12 w-full max-w-sm mx-auto">
+      <ul class="flex items-center justify-between w-full">
+        <li
+          :class="{
+            'text-avenue-grey': !links.prev,
+            'text-avenue-white-light cursor-pointer': links.prev,
+          }"
+          @click="prev"
+        >
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M16.2426 6.34317L14.8284 4.92896L7.75739 12L14.8285 19.0711L16.2427 17.6569L10.5858 12L16.2426 6.34317Z"
+              fill="currentColor"
+            />
+          </svg>
+        </li>
+        <li
+          :class="{
+            'text-avenue-grey': !links.next,
+            'text-avenue-white-light cursor-pointer': links.next,
+          }"
+          @click="next"
+        >
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10.5858 6.34317L12 4.92896L19.0711 12L12 19.0711L10.5858 17.6569L16.2427 12L10.5858 6.34317Z"
+              fill="currentColor"
+            />
+          </svg>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -25,6 +68,26 @@ export default {
     console.log('links', links)
     console.log('meta', meta)
     return { events, links, meta }
+  },
+
+  methods: {
+    async prev() {
+      if (!this.links.prev) return
+      this.fetchPage(this.meta.current_page - 1)
+    },
+
+    async next() {
+      if (!this.links.next) return
+      this.fetchPage(this.meta.current_page + 1)
+    },
+
+    async fetchPage(page) {
+      const { data: events, links, meta } = await this.$api.events.list({ page })
+
+      this.events = events
+      this.links = links
+      this.meta = meta
+    },
   },
 }
 </script>
