198715436c32abf319cfdb8e241a547bd093fcfe
Javier Martinez <javi@64robots.com>
2020-10-15 08:28:25 +0200

    mobile menu

--- components/commons/Navbar.vue
+++ components/commons/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="w-full flex py-5 px-2 md:px-8 bg-theavenue-background-light">
+  <div class="w-full flex py-5 md:px-8 bg-theavenue-background-light">
     <div class="flex-1 flex items-center uppercase">
       <nuxt-link
         tag="h1"
         :to="{ name: 'index' }"
-        class="text-avenue-white-light font-library uppercase text-2xl text-light-white cursor-pointer"
+        class="pl-2 md:ml-0 text-avenue-white-light font-library uppercase text-2xl text-light-white cursor-pointer"
       >
         The avenue
       </nuxt-link>
@@ -12,17 +12,17 @@
     <nav class="hidden lg:flex items-center text-sm">
       <MainMenu class="space-x-5" />
     </nav>
-    <IcMenu class="lg:hidden" />
+    <MobileMenu class="lg:hidden" />
   </div>
 </template>
 <script>
 import MainMenu from '@/components/commons/MainMenu'
-import IcMenu from '@/assets/svg/hamburger.svg?inline'
+import MobileMenu from '@/components/commons/MobileMenu'
 
 export default {
   name: 'Navbar',
 
-  components: { MainMenu, IcMenu },
+  components: { MainMenu, MobileMenu },
 
   methods: {
     async logout() {
