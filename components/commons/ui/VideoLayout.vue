f8331d59d9683f9d4e55f280a313fc534efc24d5
Javier Martinez <javi@64robots.com>
2020-10-19 17:25:51 +0200

    share video layout between video playback and video streaming

---
+++ components/commons/ui/VideoLayout.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="w-full grid grid-cols-1 md:grid-cols-9 pl-4 bg-theavenue-background-extra-light ">
+    <div class="col-span-6 flex flex-col justify-between">
+      <div :style="videoStyle">
+        <slot />
+      </div>
+      <div class="pb-4 pt-8 bg-theavenue-background-dark px-4 flex justify-between items-center">
+        <ArtistAvatar :artist="artist" size="sm" />
+        <template v-if="event">
+          <div class="flex space-x-4 items-center">
+            <SocialNetworkIcon
+              v-for="socialNetwork in event.social_media_users"
+              :key="socialNetwork.id"
+              :social-network="socialNetwork.social_media_slug"
+              class="w-6 h-6 ta-hidden md:inline-block"
+            />
+            <a
+              :href="event.talent.merchandise_url"
+              class="font-library text-lg text-light-white flex space-x-4 items-center border border-theavenue-white px-2 rounded-md py-0.5"
+              style="box-shadow: 0px 0px 10px #FFFFFF;"
+            >
+              <IcAnchorArrow />
+              website
+            </a>
+          </div>
+        </template>
+      </div>
+    </div>
+    <div class="col-span-3 flex flex-col">
+      <slot name="streaming" />
+      <template v-if="event">
+        <TipJars class="md:h-96" :event="event" @click:jar="openDonationModal" />
+        <ChatRoom class="flex-1" :event="event" :style="chatStyle" @click:jar="openDonationModal" />
+      </template>
+      <div v-else class="p-4 text-center">
+        The event isn't live. When its time will appear the tip jars and the jars. Refresh
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ArtistAvatar from '@/components/artists/ArtistAvatar'
+import SocialNetworkIcon from '@/components/commons/ui/SocialNetworkIcon.js'
+import IcAnchorArrow from '@/assets/svg/anchor_arrow.svg?inline'
+
+export default {
+  name: 'VideoLayout',
+
+  components: {
+    ArtistAvatar,
+    SocialNetworkIcon,
+    IcAnchorArrow,
+  },
+
+  props: {
+    event: {
+      type: Object,
+      default: null,
+    },
+
+    talent: {
+      type: Object,
+      default: null,
+    },
+  },
+
+  computed: {
+    videoStyle() {
+      return { height: 'calc(100vh - 78px - 96px)' }
+    },
+
+    chatStyle() {
+      const streamButtonHeight = this.talent ? 104 : 0
+      return { height: `calc(100vh - 384px - 78px - ${streamButtonHeight}px)` }
+    },
+
+    artist() {
+      return this.talent || this.event.talent
+    },
+  },
+
+  methods: {
+    openDonationModal(jar) {
+      if (!this.$auth.loggedIn) return
+
+      this.$modal.show('streaming-donate-modal', { event: this.event.name, jar })
+    },
+  },
+}
+</script>
--- pages/event/_slug.vue
+++ pages/event/_slug.vue
@@ -1,37 +1,10 @@
 <template>
-  <div class="w-full grid grid-cols-1 md:grid-cols-9">
-    <div class="col-span-6 flex flex-col justify-between">
-      <div id="streaming" ref="streaming" class="relative" />
-      <div class="pb-4 pt-8 bg-theavenue-background-dark px-4 flex justify-between items-center">
-        <ArtistAvatar :artist="event.talent" size="sm" />
-        <div class="flex space-x-4 items-center">
-          <SocialNetworkIcon
-            v-for="socialNetwork in event.social_media_users"
-            :key="socialNetwork.id"
-            :social-network="socialNetwork.social_media_slug"
-            class="w-6 h-6 ta-hidden md:inline-block"
-          />
-          <a
-            :href="event.talent.merchandise_url"
-            class="font-library text-lg text-light-white flex space-x-4 items-center border border-theavenue-white px-2 rounded-md py-0.5"
-            style="box-shadow: 0px 0px 10px #FFFFFF;"
-          >
-            <IcAnchorArrow />
-            website
-          </a>
-        </div>
-      </div>
-    </div>
-    <div class="col-span-3 flex flex-col">
-      <TipJars class="md:h-96" :event="event" @click:jar="openDonationModal" />
-      <ChatRoom class="flex-1" :event="event" :style="chatStyle" @click:jar="openDonationModal"/>
-    </div>
-  </div>
+  <VideoLayout :event="event">
+    <div id="streaming" ref="streaming" class="relative bg-theavenue-black h-full w-full" />
+  </VideoLayout>
 </template>
 <script>
-import ArtistAvatar from '@/components/artists/ArtistAvatar'
-import SocialNetworkIcon from '@/components/commons/ui/SocialNetworkIcon.js'
-import IcAnchorArrow from '@/assets/svg/anchor_arrow.svg?inline'
+import VideoLayout from '@/components/commons/ui/VideoLayout'
 
 export default {
   name: 'EventPage',
@@ -40,11 +13,7 @@ export default {
 
   layout: 'event',
 
-  components: {
-    ArtistAvatar,
-    SocialNetworkIcon,
-    IcAnchorArrow,
-  },
+  components: { VideoLayout },
 
   async asyncData({ $api, redirect, params }) {
     if (!params.slug) redirect('/')
@@ -67,12 +36,6 @@ export default {
     }
   },
 
-  computed: {
-    chatStyle() {
-      return { height: 'calc(100vh - 384px - 78px)' }
-    },
-  },
-
   mounted() {
     this.videoHeight =
       (this.dimensions.height / this.dimensions.width) * this.$refs.streaming.offsetWidth
@@ -81,12 +44,6 @@ export default {
       width: '100%',
       height: this.videoHeight,
     })
-  },
-
-  methods: {
-    openDonationModal(jar) {
-      this.$modal.show('streaming-donate-modal', { event: this.event.name, jar })
-    },
   },
 }
 </script>
