616ef4ccddf0af7765b3f7179c2564ad15d268f9
Javier Martinez <javi@64robots.com>
2020-10-22 16:21:27 +0200

    report modal

---
+++ components/talents/modals/ReportModal.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="relative">
+    <div class="absolute text-theavenue-white top-0 right-0 mt-4 mr-6">
+      <IcClose class="cursor-pointer" @click="close" />
+    </div>
+    <div class="px-6 pb-5 pt-10 bg-theavenue-background">
+      <p class="text-2xl leading-tight font-medium">Report Video</p>
+    </div>
+    <form class="mt-3 px-6 pb-10 pt-5" @submit.prevent="report">
+      <div v-for="reason in reasons" :key="reason.value" class="mb-6">
+        <R64Radio
+          :value="form.reason"
+          :label="reason.label"
+          :radio-value="reason.value"
+          name="reason"
+          @click="form.reason = $event"
+        />
+      </div>
+      <div class="mb-6">
+        <R64Input v-model="form.message" full label="Reason" />
+      </div>
+
+      <R64Button
+        type="submit"
+        class="mt-8"
+        full
+        :loading="busy"
+        :disabled="!form.reason || !form.message"
+      >
+        Confirm
+      </R64Button>
+    </form>
+  </div>
+</template>
+<script>
+import IcClose from '@/assets/svg/close_2.svg?inline'
+
+export default {
+  name: 'ReportModal',
+
+  components: { IcClose },
+
+  props: {
+    talent: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  async fetch() {
+    const { data } = await this.$api.talent.reportReasons()
+    this.reasons = data
+  },
+
+  data() {
+    return {
+      busy: false,
+      reasons: [],
+      form: {
+        reason: '',
+        message: '',
+      },
+    }
+  },
+
+  methods: {
+    async report() {
+      this.busy = true
+      await this.$api.talent.report(this.talent.id, this.form)
+      this.busy = false
+      this.close()
+    },
+
+    close() {
+      this.$modal.hide('report-modal')
+    },
+  },
+}
+</script>
