8a2514fc115ee05417f5f6eb55394546268be9fe
Pepe García <pgarciag93@gmail.com>
2020-11-05 12:21:51 +0100

    Add custom controls to video

--- components/events/EventVideo.vue
+++ components/events/EventVideo.vue
@@ -1,16 +1,44 @@
 <template>
-  <video
-    id="streaming"
-    ref="streaming"
-    controls
-    controlsList="nofullscreen"
-    class="relative bg-theavenue-black h-full w-full outline-none"
-  />
+  <div class="relative h-full w-full">
+    <video
+      id="streaming"
+      ref="streaming"
+      controls
+      controlsList="nofullscreen"
+      class="relative bg-theavenue-black h-full w-full outline-none"
+    />
+    <div class="dimmer-gradient-small w-full absolute top-0 left-0 h-full" />
+    <IcPause
+      v-if="play"
+      class="cursor-pointer w-10 h-10 absolute left-0 bottom-0 ml-6 mb-4"
+      @click="pauseVideo"
+    />
+    <IcPlay
+      v-else
+      class="cursor-pointer w-16 h-16 absolute left-0 bottom-0 ml-6 mb-4"
+      @click="playVideo"
+    />
+    <IcVolume
+      class="cursor-pointer w-10 h-10 absolute right-0 bottom-0 mr-6 mb-4"
+      :class="{ 'opacity-50': muted }"
+      @click="toggleMute"
+    />
+  </div>
 </template>
 
 <script>
+import IcPlay from '@/assets/svg/play.svg?inline'
+import IcPause from '@/assets/svg/pause.svg?inline'
+import IcVolume from '@/assets/svg/volume_up.svg?inline'
+
 export default {
   name: 'EventVideo',
+
+  components: {
+    IcPlay,
+    IcPause,
+    IcVolume,
+  },
 
   props: {
     playbackId: {
@@ -19,18 +47,50 @@ export default {
     },
   },
 
-  mounted() {
-    const url = `https://stream.mux.com/${this.playbackId}.m3u8`
-    const video = this.$refs.streaming
+  data() {
+    return {
+      play: true,
+      muted: false,
+      video: null,
+    }
+  },
 
-    if (video && video.canPlayType('application/vnd.apple.mpegurl')) {
-      video.src = url
+  mounted() {
+    // const url = `https://stream.mux.com/${this.playbackId}.m3u8`
+    const url = 'https://stream.mux.com/YNoCxNCLmwyb5ECtug17B1Xoqv02T02EzZS01hFX3xqO2Y.m3u8'
+    this.video = this.$refs.streaming
+
+    if (this.video && this.video.canPlayType('application/vnd.apple.mpegurl')) {
+      this.video.src = url
     } else if (Hls.isSupported()) {
       const hls = new Hls()
       hls.loadSource(url)
-      hls.attachMedia(video)
+      hls.attachMedia(this.video)
     }
-    video.play()
+    this.video.play()
+    this.video.controls = false
+  },
+
+  methods: {
+    playVideo() {
+      this.play = true
+      this.video.play()
+    },
+
+    pauseVideo() {
+      this.play = false
+      this.video.pause()
+    },
+
+    toggleMute() {
+      this.muted = !this.muted
+      this.video.muted = this.muted
+    },
   },
 }
 </script>
+<style scoped>
+.dimmer-gradient-small {
+  background: linear-gradient(0deg, rgba(0, 0, 0, 1) 0%, rgba(255, 255, 255, 0) 8%);
+}
+</style>
