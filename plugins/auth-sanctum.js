17b3c61f2921df171c73df40c955989eef72a7a5
Javier Martinez <javi@64robots.com>
2020-11-09 16:46:28 +0100

    debug sanctum errors

--- plugins/auth-sanctum.js
+++ plugins/auth-sanctum.js
@@ -1,24 +1,20 @@
 export default function({ $axios }) {
   $axios.onError(async error => {
-    try {
-      const code = parseInt(error.response.status)
-      if (code === 419 && !error.config.__isRetryRequest) {
-        error.config.__isRetryRequest = true
+    console.error(error)
+    const code = parseInt(error.response.status)
+    if (code === 419 && !error.config.__isRetryRequest) {
+      error.config.__isRetryRequest = true
 
-        await $axios.get('/sanctum/csrf-cookie', {
-          headers: {
-            'X-Requested-With': 'XMLHttpRequest',
-          },
-          withCredentials: true,
-        })
+      await $axios.get('/sanctum/csrf-cookie', {
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest',
+        },
+        withCredentials: true,
+      })
 
-        return $axios.request(error.config)
-      }
-      console.error(error)
-      throw error
-    } catch (error) {
-      console.error(error)
-      throw error
+      return $axios.request(error.config)
     }
+
+    throw error
   })
 }
