df65bd1b59de32c9c4b4057264e7e30aeb01c78d
Pepe García <pgarciag93@gmail.com>
2020-11-13 12:59:22 +0100

    Fix mobile menu

--- components/commons/MobileMenu.vue
+++ components/commons/MobileMenu.vue
@@ -12,25 +12,101 @@
         </nuxt-link>
         <IcClose />
       </div>
-      <MainMenu class="flex flex-col items-center space-y-5 mt-5" @click.native="open = false" />
+      <div class="flex flex-col items-center space-y-5 mt-5">
+        <a
+          v-for="category in categories"
+          :key="category.name"
+          :href="`/?category=${category.id}`"
+          :to="{ name: 'index', query: { category: category.id } }"
+          class="uppercase text-avenue-white-light font-library text-2xl hover:text-light-white mt-1 focus:outline-none cursor-pointer"
+          @click.prevent="setCategory(category)"
+        >
+          {{ category.name }}
+        </a>
+        <div v-if="$auth.loggedIn" class="flex flex-col items-center space-y-5">
+          <span
+            class="uppercase text-theavenue-turquoise-neon font-library text-2xl hover:text-light-turquoise mt-1 focus:outline-none"
+          >
+            {{ $auth.user.name }}
+          </span>
+          <button
+            v-if="$auth.user.has_confirmed_talent"
+            class="uppercase text-avenue-white-light font-library text-2xl hover:text-light-white mt-1 focus:outline-none cursor-pointer"
+            @click="goToDashboard"
+          >
+            Dashboard
+          </button>
+          <button
+            v-if="!$auth.user.talent_id"
+            class="uppercase text-avenue-white-light font-library text-2xl hover:text-light-white mt-1 focus:outline-none cursor-pointer"
+            @click="startStreaming"
+          >
+            Start Streaming
+          </button>
+          <button
+            v-if="$auth.user.talent_id && !$auth.user.has_confirmed_talent"
+            class="uppercase text-avenue-white-light font-library text-2xl hover:text-light-white mt-1 focus:outline-none cursor-pointer"
+            @click="completeStripe"
+          >
+            Complete Profile
+          </button>
+          <button
+            class="uppercase text-avenue-white-light font-library text-2xl hover:text-light-white mt-1 focus:outline-none cursor-pointer"
+            @click="logout"
+          >
+            Log out
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import MainMenu from '@/components/commons/MainMenu'
+import { mapState } from 'vuex'
 import IcMenu from '@/assets/svg/hamburger.svg?inline'
 import IcClose from '@/assets/svg/close.svg?inline'
 
 export default {
   name: 'MobileMenu',
 
-  components: { MainMenu, IcMenu, IcClose },
+  components: { IcMenu, IcClose },
 
   data() {
     return {
       open: false,
     }
+  },
+
+  computed: {
+    ...mapState({
+      categories: state => state.global.categories,
+    }),
+  },
+
+  methods: {
+    goToDashboard() {
+      window.open(this.$config.baseURL)
+    },
+
+    async logout() {
+      await this.$auth.logout()
+    },
+
+    setCategory(category) {
+      this.open = false
+      this.$router.push({ name: 'index', query: { category: category.id } })
+    },
+
+    async completeStripe() {
+      const { data: url } = await this.$api.talent.stripeAuthorize(this.$auth.user.talent_id)
+      window.location.href = url
+    },
+
+    startStreaming() {
+      this.open = false
+      this.$modal.show('talent-signup-modal')
+    },
   },
 }
 </script>
