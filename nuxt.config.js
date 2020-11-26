6c50618b195b5d3153ccc738a6cde1874d0a8a6b
Pepe García <pgarciag93@gmail.com>
2020-11-26 10:04:32 +0100

    viewport fit cover

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
     title: 'The aVenue',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, viewport-fit=cover' },
       {
         hid: 'description',
         name: 'description',
