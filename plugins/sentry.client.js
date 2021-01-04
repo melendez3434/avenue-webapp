9b1d1459a19fc2072dcbd40258924196e7176e96
Javier Martinez <javi@64robots.com>
2021-01-04 14:15:14 +0100

    remove console log

--- plugins/sentry.client.js
+++ plugins/sentry.client.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import * as Sentry from '@sentry/vue'
 
 export default function({ $config }) {
-  console.log($config.sentryEnabled)
-  console.log($config.sentryDSN)
   if ($config.sentryEnabled) {
     console.log('hey')
     Sentry.init({
