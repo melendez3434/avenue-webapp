647f1d6e505a9a80f4ef7e56b57694cc14de7dfc
Javier Martinez <javi@64robots.com>
2021-01-15 23:19:59 +0100

    wip

--- pages/broadcast/_id.vue
+++ pages/broadcast/_id.vue
@@ -238,7 +238,7 @@ export default {
         this.mediaRecorder.stop()
         setTimeout(() => {
           this.startStreaming()
-        }, 1000)
+        }, 5000)
       })
 
       this.socket.on(`${this.talent.stream_key}-processed-chunk`, chunkId => {
