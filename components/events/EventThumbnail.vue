c1abb18972dc33ab08b4a6136d6d869c51789e1b
Cecilia Olivera <ceci.coding@gmail.com>
2021-01-20 11:06:09 +0100

    made new component for thumbnail

---
+++ components/events/EventThumbnail.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="w-48 h-28 mr-6 flex items-center justify-center overflow-hidden">
+    <img v-if="event.thumbnail" :src="event.thumbnail" alt="Event thumbnail" />
+    <img
+      v-else-if="hasAssets"
+      :src="`https://image.mux.com/${event.assets[0].playback_id}/thumbnail.jpg`"
+      alt="Event thumbnail"
+    />
+    <Placeholder v-else title="Event thumbnail" class="h-full " />
+  </div>
+</template>
+
+<script>
+import Placeholder from '@/assets/svg/thumbnail_placeholder.svg?inline'
+
+export default {
+  components: { Placeholder },
+
+  props: {
+    event: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
