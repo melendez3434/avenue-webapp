853425b501fb4598ef48e76eadd3cf9af4178f0e
Cecilia Olivera <ceci.coding@gmail.com>
2021-01-21 12:56:02 +0100

    added transition element

--- components/events/OtherLiveEvents.vue
+++ components/events/OtherLiveEvents.vue
@@ -1,17 +1,23 @@
 <template>
-  <section class="bg-avenue-blue-light p-6 h-72 relative">
-    <IcClose aria-role="button" aria-label="close" class="absolute top-4 right-4" />
-    <header>
-      <h2 class="text-2xl mb-3">Stay Tuned!</h2>
-      <p>This streaming is about to end but these events are live now</p>
-    </header>
-    <ul v-for="event in events" :key="event.id">
-      <li class="h-3/6 w-40 flex flex-col justify-center items-center">
-        <EventThumbnail :event="event" />
-        <span>{{ event.talent.name }}</span>
-      </li>
-    </ul>
-  </section>
+  <transition name="fade">
+    <section class="bg-avenue-blue-light p-6 h-72 fixed bottom-0 z-10">
+      <IcClose
+        aria-role="button"
+        aria-label="close"
+        class="absolute top-4 right-4 cursor-pointer"
+      />
+      <header>
+        <h2 class="text-2xl mb-3">Stay Tuned!</h2>
+        <p>This streaming is about to end but these events are live now</p>
+      </header>
+      <ul v-for="event in events" :key="event.id">
+        <li class="h-3/6 w-40 flex flex-col justify-center items-center">
+          <EventThumbnail :event="event" />
+          <span>{{ event.talent.name }}</span>
+        </li>
+      </ul>
+    </section>
+  </transition>
 </template>
 
 <script>
@@ -28,3 +34,8 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.fade-enter-active {
+}
+</style>
