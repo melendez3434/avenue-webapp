91a6cd6b5597401c06e9d7ac79f22fea688d0643
Cecilia Olivera <ceci.coding@gmail.com>
2021-01-21 11:23:20 +0100

    added loop with thumbnail

--- components/events/OtherLiveEvents.vue
+++ components/events/OtherLiveEvents.vue
@@ -1,10 +1,16 @@
 <template>
-  <section class="bg-avenue-blue-light p-6 h-80">
-    <IcClose aria-role="button" aria-label="close" />
+  <section class="bg-avenue-blue-light p-6 h-72 relative">
+    <IcClose aria-role="button" aria-label="close" class="absolute top-4 right-4" />
     <header>
-      <h2>Stay Tuned!</h2>
+      <h2 class="text-2xl mb-3">Stay Tuned!</h2>
       <p>This streaming is about to end but these events are live now</p>
     </header>
+    <ul v-for="event in events" :key="event.id">
+      <li class="h-3/6 w-40 flex flex-col justify-center items-center">
+        <EventThumbnail :event="event" />
+        <span>{{ event.talent.name }}</span>
+      </li>
+    </ul>
   </section>
 </template>
 
