cc1cb040a999fd0e1e65560a6703df0a6fd2ad5c
Cecilia Olivera <ceci.coding@gmail.com>
2021-01-22 13:06:20 +0100

    corrected nested query

--- pages/event/_slug.vue
+++ pages/event/_slug.vue
@@ -38,7 +38,7 @@ export default {
       const { data: event } = await $api.events.get(params.slug)
       const selectedAsset = event.assets.length ? event.assets[0].playback_id : ''
       const { data: otherLiveEvents } = await $api.events.list({
-        talent: { streaming_status: 'active' },
+        live: true,
       })
       return { event, selectedAsset, otherLiveEvents }
     } catch (e) {
