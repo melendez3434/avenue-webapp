acd3bff1d4a888959498462f38d6b1322dc094c3
Ceci Olivera <ceci.coding@gmail.com>
2021-01-26 17:22:19 +0100

    Bring back new idle screen

--- pages/event/_slug.vue
+++ pages/event/_slug.vue
@@ -1,23 +1,27 @@
 <template>
-  <section>
-    <VideoLayout :event="event">
-      <EventVideo
-        :key="activePlaybackId"
-        :playback-id="activePlaybackId"
-        :offline="isFinished"
-        :assets="event.assets"
-        @selected-asset="selectedAsset = $event"
-      />
-      <template #placeholder>
-        <div
-          class="w-full h-full flex items-start pt-20 md:pt-0 md:items-center justify-center text-center"
-        >
-          Streaming is not live yet or it is idle. When its live you'll watch it here.
-        </div>
-      </template>
-    </VideoLayout>
-    <OtherLiveEvents v-if="showOtherEvents" :events="otherLiveEvents" @close="closeModal" />
-  </section>
+  <VideoLayout :event="event">
+    <EventVideo
+      :key="activePlaybackId"
+      :playback-id="activePlaybackId"
+      :offline="isFinished"
+      :assets="event.assets"
+      @selected-asset="selectedAsset = $event"
+    />
+    <template #placeholder>
+      <div
+        class="w-full h-screen flex flex-col md:items-center justify-center bg-no-repeat bg-cover bg-center pt-20 md:pt-0 text-center font-league-gothic uppercase text-2xl tracking-wide"
+        :style="{
+          backgroundImage: `linear-gradient(
+			      rgba(0, 0, 0, 0.85),
+			      rgba(0, 0, 0, 0.9)
+		        ),url(${event.talent.cover_photo})`,
+        }"
+      >
+        <p>{{ event.talent.name }}'s stream is not live yet or it is idle.</p>
+        <p>When it's live you'll see it here.</p>
+      </div>
+    </template>
+  </VideoLayout>
 </template>
 <script>
 import spacetime from 'spacetime'
@@ -37,30 +41,16 @@ export default {
     try {
       const { data: event } = await $api.events.get(params.slug)
       const selectedAsset = event.assets.length ? event.assets[0].playback_id : ''
+
       return { event, selectedAsset }
     } catch (e) {
       redirect('/')
     }
   },
 
-  data() {
-    return {
-      showModal: false,
-      otherEvents: [],
-    }
-  },
-
   computed: {
     isFinished() {
       return this.event.is_finished
-    },
-
-    showOtherEvents() {
-      return this.showModal && this.otherLiveEvents.length > 0
-    },
-
-    otherLiveEvents() {
-      return this.otherEvents.slice(0, 4).filter(event => event.id != this.event.id)
     },
 
     activePlaybackId() {
@@ -80,32 +70,6 @@ export default {
     timezoneFormatted() {
       const [, city] = this.event.timezone.split('/')
       return city.replace('_', ' ')
-    },
-  },
-
-  mounted() {
-    this.$echo.channel(`event.${this.event.id}`).listen('EventIsEndedNow', () => {
-      this.fetchOtherLiveEvents()
-    })
-  },
-
-  beforeDestroy() {
-    this.$echo.channel(`event.${this.event.id}`).stopListening('EventIsEndedNow')
-  },
-
-  methods: {
-    closeModal() {
-      return (this.showModal = false)
-    },
-
-    async fetchOtherLiveEvents() {
-      try {
-        const { data: otherEvents } = await this.$api.events.list({ live: true })
-        this.otherEvents = otherEvents
-        this.showModal = true
-      } catch {
-        console.error("Couldn't fetch other live events")
-      }
     },
   },
 
