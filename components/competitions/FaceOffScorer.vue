7993a2399402abf0353b6af470d0a401dc19ff64
Cecilia Olivera <ceci.coding@gmail.com>
2021-03-10 16:35:52 +0100

    made all fours scores cards look the same

--- components/competitions/FaceOffScorer.vue
+++ components/competitions/FaceOffScorer.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="border border-theavenue-yellow-neon rounded-lg p-6 flex flex-col"
+    class="border border-theavenue-yellow-neon rounded-lg p-6 flex flex-col mb-4 md:mb-0"
     style="box-shadow: 0px 0px 10px rgba(255, 210, 67);"
   >
     <div class="flex items-center mb-4">
@@ -8,12 +8,12 @@
         v-if="talent.talent.photo"
         :src="talent.talent.photo"
         :alt="`${talent.name}`"
-        class="rounded-full w-10 h-10 mr-4"
+        class="rounded-full w-12 h-12 mr-4"
       />
-      <div v-else class="bg-gray-300 rounded-full w-10 h-10 mr-4" />
+      <div v-else class="bg-gray-300 rounded-full w-12 h-12 mr-4" />
       <span class="font-bold">{{ talent.name }}</span>
     </div>
-    <blockquote class="italic">"{{ talent.motivation }}"</blockquote>
+    <blockquote class="italic h-12 overflow-hidden">{{ talent.motivation }}...</blockquote>
     <span
       class="mt-3 font-league-gothic uppercase text-lg justify-self-center text-center tracking-wider"
     >
