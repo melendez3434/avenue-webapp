718f7a3cec7a8d601ec9c8428e9455a13fab1f03
Cecilia Olivera <ceci.coding@gmail.com>
2021-05-11 17:27:37 +0200

    adjusted sizes for wider screens :iphone:

--- components/commons/Hero.vue
+++ components/commons/Hero.vue
@@ -2,18 +2,26 @@
   <div class="w-full relative">
     <img src="@/assets/images/hero.jpg" alt="man recording music on a studio" class="w-full" />
     <div class="absolute top-6 md:top-1/4 p-2 md:p-6 lg:p-10">
-      <h1 class="font-bold text-lg md:text-3xl lg:text-6xl leading-none	">Watch Live</h1>
-      <h1 class="font-bold text-lg md:text-3xl lg:text-6xl leading-none	">Performances.</h1>
-      <h1 class="font-bold text-lg md:text-3xl lg:text-6xl leading-none	">No Cost to Perform.</h1>
-      <h1 class="font-bold text-lg md:text-3xl lg:text-6xl leading-none	">No Cost to Watch.</h1>
-      <div class="mt-10 space-x-6">
+      <h1 class="font-bold text-lg md:text-3xl lg:text-6xl 3xl:text-xxl leading-none	">
+        Watch Live
+      </h1>
+      <h1 class="font-bold text-lg md:text-3xl lg:text-6xl 3xl:text-xxl leading-none	">
+        Performances.
+      </h1>
+      <h1 class="font-bold text-lg md:text-3xl lg:text-6xl 3xl:text-xxl leading-none	">
+        No Cost to Perform.
+      </h1>
+      <h1 class="font-bold text-lg md:text-3xl lg:text-6xl 3xl:text-xxl leading-none	">
+        No Cost to Watch.
+      </h1>
+      <div class="mt-4 md:mt-10 space-x-2 md:space-x-6">
         <button
-          class="text-white font-semibold text-lg md:text-xl lg:text-2xl py-4 px-6 bg-theavenue-turquoise-neon transform scale-100 hover:scale-105"
+          class="text-white font-semibold text-base md:text-xl lg:text-2xl 3xl:text-3xl py-2 px-4 md:py-4 md:px-6 bg-theavenue-turquoise-neon transform scale-100 hover:scale-105"
         >
           Start Watching
         </button>
         <button
-          class="text-white font-semibold text-lg md:text-xl lg:text-2xl py-4 px-6 bg-theavenue-blue-neon transform scale-100 hover:scale-105"
+          class="text-white font-semibold text-base md:text-xl lg:text-2xl 3xl:text-3xl py-2 px-4 md:py-4 md:px-6 bg-theavenue-blue-neon transform scale-100 hover:scale-105"
           @click="openSignup()"
         >
           Start Performing
