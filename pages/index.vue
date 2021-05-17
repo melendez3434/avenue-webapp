96884208bca1394de7bf8c771117323183209d1d
Cecilia Olivera <ceci.coding@gmail.com>
2021-05-17 21:40:11 +0200

    resolved conflicts

--- pages/index.vue
+++ pages/index.vue
@@ -57,7 +57,7 @@ import { mapState } from 'vuex'
 import spacetime from 'spacetime'
 import CompetitionMarqueeItem from '@/components/events/CompetitionMarqueeItem'
 import LiveEventListItem from '@/components/events/LiveEventListItem'
-import Hero from '@/components/commons/Hero'
+import Logolights from '@/components/commons/Logolights'
 import CompetitionModalAnnouncement from '@/components/competitions/CompetitionModalAnnouncement'
 
 export default {
@@ -67,7 +67,7 @@ export default {
 
   components: {
     LiveEventListItem,
-    Hero,
+    Logolights,
     CompetitionModalAnnouncement,
     CompetitionMarqueeItem,
   },
