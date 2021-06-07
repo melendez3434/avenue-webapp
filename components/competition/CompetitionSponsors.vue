1823de32b051afd1aa3519d35b0f411c63554525
Javier Martinez <javi@64robots.com>
2021-06-07 16:38:03 +0200

    sponsors responsive

--- components/competition/CompetitionSponsors.vue
+++ components/competition/CompetitionSponsors.vue
@@ -1,18 +1,19 @@
 <template>
-  <section class="container md:mx-auto my-20">
+  <section class="container md:mx-auto">
     <div class="flex flex-row gap-4 items-start mb-6">
       <IcSponsor class="h-8" />
       <h2 class="text-xl font-bold">Sponsors</h2>
     </div>
-    <div class="mt-5 grid grid-cols-2 md:grid-cols-3 gap-6 lg:gap-12 items-end mb-10 md:mb-0 ">
-      <div v-for="sponsor in sponsors" :key="sponsor.id" class="h-full">
-        <img
-          v-if="sponsor.logo"
-          :src="sponsor.logo"
-          :alt="`${sponsor.name} logo`"
-          class="h-1/2 xl:h-full"
-        />
-      </div>
+    <div
+      class="flex flex-col md:flex-row flex-wrap space-y-4 justify-center md:justify-between md:items-center mt-5 w-full"
+    >
+      <img
+        v-for="sponsor in sponsors"
+        :key="sponsor.id"
+        :src="sponsor.logo"
+        :alt="`${sponsor.name} logo`"
+        class="sponsor-item"
+      />
     </div>
   </section>
 </template>
@@ -33,3 +34,14 @@ export default {
   },
 }
 </script>
+<style scoped>
+.sponsor-item {
+  width: 100%;
+}
+@media (min-width: 768px) {
+  .sponsor-item {
+    width: calc(30% - 0.5rem);
+    margin-right: 0.5rem;
+  }
+}
+</style>
