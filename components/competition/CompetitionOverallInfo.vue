405e9c831dc7c0438ac845842a813ca118a5e74b
Cecilia Olivera <ceci.coding@gmail.com>
2021-06-07 16:51:42 +0200

    finished overall info

--- components/competition/CompetitionOverallInfo.vue
+++ components/competition/CompetitionOverallInfo.vue
@@ -1,55 +1,91 @@
 <template>
   <div>
-    <section class="md:grid grid-cols-3 grid-rows-2 gap-12 mx-auto mt-12 container">
-      <div class="flex flex-col items-center justify-center gap-6 col-end-3 row-end-2">
-        <h4 class="font-league-gothic uppercase text-2xl lg:text-3xl">
-          Grand prize total
-        </h4>
-        <IcTrophy class="h-20" />
-        <span class="text-4xl lg:text-5xl font-league-gothic ">
-          {{ info.grand_prize_status }}
-        </span>
+    <section>
+      <div
+        v-if="topScorer && lastWeekWinner"
+        class="md:grid grid-cols-3 grid-rows-2 gap-12 mx-auto mt-12 container"
+      >
+        <div class="flex flex-col items-center justify-center gap-6 col-end-3 row-end-2">
+          <h4 class="font-league-gothic uppercase text-2xl lg:text-3xl">
+            Grand prize total
+          </h4>
+          <IcTrophy class="h-20" />
+          <span class="text-4xl lg:text-5xl font-league-gothic ">
+            {{ info.grand_prize_status }}
+          </span>
+        </div>
+        <div
+          class="col-start-2 flex flex-col items-center justify-center gap-6 mt-6 col-end-3 row-end-3"
+        >
+          <h4 class="font-league-gothic uppercase text-2xl lg:text-3xl">
+            Current Round's Prize Total
+          </h4>
+          <IcPodium class="h-20" />
+          <span class="text-4xl lg:text-5xl font-league-gothic">
+            {{ info.weekly_prize_status }}
+          </span>
+        </div>
+        <div class="flex flex-col items-center justify-center gap-6 md:mt-32 col-end-2 row-end-2">
+          <h4 class="font-league-gothic uppercase text-2xl lg:text-3xl">
+            Last week's winner
+          </h4>
+          <img
+            v-if="lastWeekWinner.competition_talent.talent.photo"
+            :src="lastWeekWinner.competition_talent.talent.photo"
+            :alt="lastWeekWinner.competition_talent.name"
+            class="rounded-full w-24 h-24"
+          />
+          <span v-if="lastWeekWinner.name" class="text-xs font-bold">
+            {{ lastWeekWinner.competition_talent.name }}
+          </span>
+        </div>
+        <div class="flex flex-col items-center justify-center gap-6 md:mt-32 col-end-4 row-end-2">
+          <h4 class="font-league-gothic uppercase text-2xl lg:text-3xl">
+            Top scorer
+          </h4>
+          <img
+            v-if="topScorer.competition_talent.talent.photo"
+            :src="topScorer.competition_talent.talent.photo"
+            :alt="`${topScorer.competition_talent.name}`"
+            class="rounded-full w-24 h-24"
+          />
+          <span class="text-xs font-bold">{{ topScorer.competition_talent.name }}</span>
+        </div>
       </div>
       <div
-        class="col-start-2 flex flex-col items-center justify-center gap-6 mt-6 col-end-3 row-end-3"
+        v-if="!lastWeekWinner"
+        class="flex flex-no-wrap flex-col lg:flex-row justify-between gap-12 mx-auto mt-12 container max-w-screen-lg"
       >
-        <h4 class="font-league-gothic uppercase text-2xl lg:text-3xl">
-          Current Round's Prize Total
-        </h4>
-        <IcPodium class="h-20" />
-        <span class="text-4xl lg:text-5xl font-league-gothic">{{ info.weekly_prize_status }}</span>
-      </div>
-      <div
-        v-if="lastWeekWinner"
-        class="flex flex-col items-center justify-center gap-6 md:mt-24 col-end-2 row-end-2"
-      >
-        <h4 class="font-league-gothic uppercase text-2xl lg:text-3xl">
-          Last week's winner
-        </h4>
-        <img
-          v-if="lastWeekWinner.photo"
-          :src="lastWeekWinner.photo"
-          :alt="lastWeekWinner.name"
-          class="rounded-full w-24 h-24"
-        />
-        <span v-if="lastWeekWinner.name" class="text-xs font-bold">
-          {{ lastWeekWinner.name }}
-        </span>
-      </div>
-      <div
-        v-if="topScorer"
-        class="flex flex-col items-center justify-center gap-6 md:mt-24 col-end-4 row-end-2"
-      >
-        <h4 class="font-league-gothic uppercase text-2xl lg:text-3xl">
-          Top scorer
-        </h4>
-        <img
-          v-if="topScorer.photo"
-          :src="topScorer.photo"
-          :alt="`${topScorer.name}`"
-          class="rounded-full w-24 h-24"
-        />
-        <span class="text-xs font-bold">{{ topScorer.name }}</span>
+        <div class=" flex flex-col items-center justify-between gap-6">
+          <h4 class="font-league-gothic uppercase text-2xl lg:text-3xl">
+            Current Round's Prize Total
+          </h4>
+          <IcPodium class="h-20" />
+          <span class="text-4xl lg:text-5xl font-league-gothic">
+            {{ info.weekly_prize_status }}
+          </span>
+        </div>
+        <div class="flex flex-col items-center justify-between gap-6">
+          <h4 class="font-league-gothic uppercase text-2xl lg:text-3xl">
+            Grand prize total
+          </h4>
+          <IcTrophy class="h-20" />
+          <span class="text-4xl lg:text-5xl font-league-gothic ">
+            {{ info.grand_prize_status }}
+          </span>
+        </div>
+        <div v-if="topScorer" class="flex flex-col items-center justify-between gap-3">
+          <h4 class="font-league-gothic uppercase text-2xl lg:text-3xl">
+            Top scorer
+          </h4>
+          <img
+            v-if="topScorer.competition_talent.talent.photo"
+            :src="topScorer.competition_talent.talent.photo"
+            :alt="`${topScorer.competition_talent.name}`"
+            class="rounded-full w-24 h-24"
+          />
+          <span class="text-xs font-bold">{{ topScorer.competition_talent.name }}</span>
+        </div>
       </div>
     </section>
     <section v-if="info.current_top_four_scorers.length" class="container mx-auto mt-20">
@@ -107,11 +143,11 @@ export default {
 
   computed: {
     lastWeekWinner() {
-      return this.info.last_week_winners[0]
+      return this.info.last_week_winners.length ? this.info.last_week_winners[0] : null
     },
 
     topScorer() {
-      return this.info.general_top_scorers[0]
+      return this.info.general_top_scorers.length ? this.info.general_top_scorers[0] : null
     },
   },
 }
