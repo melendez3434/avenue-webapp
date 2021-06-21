08f5ffd610c5bdcb6c0d4f1a57881c623859985e
Javier Martinez <javi@64robots.com>
2021-06-21 10:58:12 +0200

    google analytics plugin

---
+++ plugins/ga.client.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import VueGtag from 'vue-gtag'
+
+export default ({ isDev, app, $config }) => {
+  if (!isDev) {
+    Vue.use(
+      VueGtag,
+      {
+        config: { id: $config.gaId },
+      },
+      app.router
+    )
+  } else {
+    console.log('Skipping GA in development')
+  }
+}
